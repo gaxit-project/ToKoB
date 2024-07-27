ecdb2332eff12f58bf4329ea8910ecc2cca97e3e
LAPTOP-KP2HKT1L\marumoto keita <keita.marumoto@mix-lab.net>
2024-07-27 13:56:10 +0900

    2024/07/27 ゲームスタートのカウントダウン、プレイヤーの移動・ジャンプ、道の自動生成

---
+++ Assets/Script/Countdown.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+ 
+public class Countdown : MonoBehaviour
+{
+	[SerializeField]
+	private TextMeshProUGUI _textCountdown;
+	public bool start=false;
+ 
+	
+	void Start()
+	{
+		_textCountdown.text = "";
+        StartCoroutine(CountdownCoroutine());
+	}
+ 
+	IEnumerator CountdownCoroutine()
+	{
+ 
+		_textCountdown.text = "3";
+		yield return new WaitForSeconds(1.0f);
+ 
+		_textCountdown.text = "2";
+		yield return new WaitForSeconds(1.0f);
+ 
+		_textCountdown.text = "1";
+		yield return new WaitForSeconds(1.0f);
+		
+		_textCountdown.text = "GO!";
+		yield return new WaitForSeconds(1.0f);
+		start=true;
+ 
+		_textCountdown.text = "";
+	}
+}
---
+++ Assets/Script/PlayerControll.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerControll : MonoBehaviour
+{
+    public float moveSpeed = 3f; // 前進速度
+    public float acceleration = 0.01f; // 加速
+    public float leftRightSpeed = 4f; // 左右移動
+    public float upwardForce = 10f; // ジャンプ力
+    private Rigidbody rb;
+    private bool isGrounded; // フラグを追加
+    private Countdown countdown; // Countdownスクリプトへの参照を追加
+    public TextMeshProUGUI countdownText; //旗に触れたら時間変更
+    private Time time; // Time スクリプトへの参照を追加
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); // このGameObjectにアタッチされているRigidbodyコンポーネントを取得
+        GameObject gameManagerObj = GameObject.Find("GameManager"); // GameManagerオブジェクトを取得
+
+        if (gameManagerObj != null)
+        {
+            countdown = gameManagerObj.GetComponent<Countdown>(); // Countdownスクリプトを取得
+            time = gameManagerObj.GetComponent<Time>(); // Timeスクリプトを取得
+        }
+        else
+        {
+            Debug.LogError("GameManager オブジェクトが見つかりません");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown != null && countdown.start) // カウントダウンが終了したかどうか
+        {
+            //-----------------移動、ジャンプのスクリプト----------------------
+            transform.Translate(Vector3.forward * UnityEngine.Time.deltaTime * moveSpeed, Space.World);
+            moveSpeed += UnityEngine.Time.deltaTime * acceleration;
+
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                transform.Translate(Vector3.left * UnityEngine.Time.deltaTime * leftRightSpeed);
+            }
+
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.Translate(Vector3.left * UnityEngine.Time.deltaTime * leftRightSpeed * -1);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded && rb != null)
+            {
+                rb.AddForce(Vector3.up * upwardForce, ForceMode.Impulse);
+                isGrounded = false; // ジャンプ後は空中にいるとする
+                Debug.Log("ジャンプ");
+            }
+        }
+    }
+
+    //-------------------ジャンプするときに地面についているかどうか-----------------------------
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Road")
+        {
+            isGrounded = true; // "Road"タグのオブジェクトと接触したときにフラグをtrueにする
+            Debug.Log("地面に触れる");
+        }
+        //----------------旗に触れたら時間を変更する--------------------------
+        if (collision.gameObject.tag == "Flag") // 旗のタグをチェック
+        {
+            if (time != null)
+            {
+                time.ResetCountdown(); // カウントダウンをリセット
+                Debug.Log("旗に触れてカウントダウンをリセット");
+            }
+            else
+            {
+                Debug.LogError("Time スクリプトが見つかりません");
+            }
+        }
+    }
+}
